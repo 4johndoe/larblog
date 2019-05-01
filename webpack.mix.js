44f5eaeaffbc4a66f8b3180c6bbe0625b03570cc
bogdan <bogdandemchenko@gmail.com>
2019-05-01 22:15:09 +0300

    admLTE

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,23 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+// mix.js('resources/js/app.js', 'public/js')
+//     .sass('resources/sass/app.scss', 'public/css');
+mix.styles([
+    'resources/admin/dist/css/bootstrap.min.css',
+    'resources/admin/dist/css/font-awesome.min.css',
+    'resources/admin/dist/css/ionicons.min.css',
+    'resources/admin/dist/css/AdminLTE.min.css',
+    'resources/admin/dist/css/skins/_all-skins.min.css',
+], 'public/css/admin.css');
+
+mix.scripts([
+    'resources/admin/dist/js/jquery.min.js',
+    'resources/admin/dist/js/bootstrap.min.js',
+    'resources/admin/dist/js/fastclick.js',
+    'resources/admin/dist/js/adminlte.min.js',
+    'resources/admin/dist/js/demo.js',
+], 'public/js/admin.js');
+
+mix.copy('resources/admin/dist/fonts', 'public/fonts');
+mix.copy('resources/admin/dist/img', 'public/img');
